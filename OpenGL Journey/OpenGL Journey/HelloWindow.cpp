da428cbeb2f75faccf678e6412ccaa1be5ef5140
Lorenzo Arnaldi <lorenzoarnaldi.os@gmail.com>
2025-03-28 21:00:03 +0100

    feat: used vbo data also as color for the fragment shader yesss

--- OpenGL Journey/OpenGL Journey/HelloWindow.cpp
+++ OpenGL Journey/OpenGL Journey/HelloWindow.cpp
@@ -10,21 +10,22 @@ const unsigned int SCR_HEIGHT = 600;
 //mo nfatti metto er colore nel vertex e lo pio in input nel fragment
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec4 aColor;\n"
 "out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"   vertexColor = vec4(0.5, 0.0, 0.0, 0.5);\n"
+"   vertexColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n" //variabile globale, può essere usata sempre e da qualsiasi shader nel program, e si setta da opengl dajee
+//"uniform vec4 ourColor;\n" //variabile globale, può essere usata sempre e da qualsiasi shader nel program, e si setta da opengl dajee
 //se lo uniform non viene mai utilizzato nel glsl, er compiler lo toglie e potrebbe rompe er cazzo sta cosaaaa
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
+"   FragColor = vertexColor;\n"
 "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -159,10 +160,11 @@ int main()
 
 
 	float vertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+	//positions			colors
+	 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // top right
+	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom left
+	-0.5f,  0.5f, 0.0f,  0.9f, 0.9f, 1.0f,// top left 
 	};
 
 	unsigned int indices[] = {
@@ -182,8 +184,11 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	
@@ -201,12 +206,14 @@ int main()
 		glUseProgram(shaderProgram);
 		//dopo che usamo er programma potemo setta gli uniform eddajeeee
 		//calcolamo er valore de verde, vedemo ndo sta er vertexColorLocation e usamo er program
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		// 
+		//float timeValue = glfwGetTime();
+		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 
 		//setto l'uniformolo
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		// 
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 		glBindVertexArray(VAO);
 
