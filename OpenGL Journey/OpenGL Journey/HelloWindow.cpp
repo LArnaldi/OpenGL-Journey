b37966c1a144e23c2ab839197e7453c93d11b3ea
Lorenzo Arnaldi <lorenzoarnaldi.os@gmail.com>
2025-03-29 20:09:32 +0100

    feat: downloaded stb_image.h as image loader

--- OpenGL Journey/OpenGL Journey/HelloWindow.cpp
+++ OpenGL Journey/OpenGL Journey/HelloWindow.cpp
@@ -1,11 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <Shader.h>
+#include <Headers/Shader.h>
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
