4be03245a643ba8c2957ab86373c11032ca4b96f
Lorenzo Arnaldi <lorenzoarnaldi.os@gmail.com>
2025-03-29 12:32:05 +0100

    feat: output the position of vertex from vert to frag and use it as color

--- OpenGL Journey/OpenGL Journey/HelloWindow.cpp
+++ OpenGL Journey/OpenGL Journey/HelloWindow.cpp
@@ -7,6 +7,13 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 
+/*	ESERCIZIO 3
+*	Output the vertex position to the fragment shader using the out keyword
+*	and set the fragment's color equal to this vertex position 
+*	(see how even the vertex position values are interpolated across the triangle). 
+*	Once you managed to do this; try to answer the following question: 
+*	why is the bottom-left side of our triangle black?: solution.
+*/
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -83,28 +90,19 @@ int main()
 
 	float vertices[] = {
 	//positions			colors
-	 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // top right
-	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
-	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom left
-	-0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// top left 
+	 -0.5f,  -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom left
+	 0.0f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // top center
+	0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom right
 	};
 
-	unsigned int indices[] = {
-		0, 1, 3,	//first triangle
-		1, 2, 3		//second triangle
-	};
-
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -142,7 +140,7 @@ int main()
 
 		glBindVertexArray(VAO);
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 
 		glfwPollEvents();
--- OpenGL Journey/OpenGL Journey/Shaders/3.3.shader.fs
+++ OpenGL Journey/OpenGL Journey/Shaders/3.3.shader.fs
@@ -3,8 +3,8 @@ out vec4 FragColor;
 
 in vec3 ourColor;
 uniform float alphaColor;
-
+in vec3 positionToColor;
 void main()
 {
-    FragColor = vec4(ourColor, alphaColor);
+    FragColor = vec4(positionToColor, alphaColor);
 }
